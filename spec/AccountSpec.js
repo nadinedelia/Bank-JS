9a513691776c274298a69aeaccc11ff99cf3b308
Nadine <nadine.loepfe@gmail.com>
2020-07-17 14:54:32 +0100

    failing test for withdraw

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -7,11 +7,17 @@ describe("Account", function() {
 
   it("should start with a balance of 0", function() {
     expect(account.balance).toEqual(0);
-});
+  });
 
   it("is possible to deposit money", function() {
     account.deposit(10);
     expect(account.balance).toEqual(10);
-
   });
+
+  it("is possible to deposit money", function() {
+    account.deposit(50)
+    account.withdraw(10);
+    expect(account.balance).toEqual(40);
+  });
+  
 });
